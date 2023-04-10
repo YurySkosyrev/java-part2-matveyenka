d706bc6f32113abdaf3e6670896a4b47151e2070
YurySkosyrev <skosyrev-87@mail.ru>
2023-04-10 23:16:46 +0300

    Step 14.7/17

---
+++ javaPart2Practice/src/main/java/barrier/RocketDetailRunnable.java
@@ -0,0 +1,28 @@
+package barrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class RocketDetailRunnable implements Runnable{
+
+    private final RocketDetail rocketDetail;
+    private final CyclicBarrier cyclicBarrier;
+
+    public RocketDetailRunnable(RocketDetail rocketDetail, CyclicBarrier cyclicBarrier) {
+        this.rocketDetail = rocketDetail;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Готовиться деталь: " + rocketDetail);
+        try {
+            Thread.sleep(1000L);
+            System.out.println("Деталь готова: " + rocketDetail);
+            cyclicBarrier.await();
+            System.out.println("Деталь использована: " + rocketDetail);
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
