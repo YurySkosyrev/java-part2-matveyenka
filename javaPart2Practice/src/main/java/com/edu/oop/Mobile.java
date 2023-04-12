6a62b3f1d87d085529b95a626f8c4f607a8454f7
YurySkosyrev <skosyrev-87@mail.ru>
2023-04-12 23:09:46 +0300

    Step 15.1/17

--- javaPart2Practice/src/main/java/com/edu/oop/Mobile.java
+++ javaPart2Practice/src/main/java/com/edu/oop/Mobile.java
@@ -1,6 +1,6 @@
 package com.edu.oop;
 
-public class Mobile extends Computer{
+public class Mobile extends Computer {
 
     public void print(){
         System.out.println("This is print from Mobile");
--- javaPart2Practice/src/main/java/com/edu/threadpool/tasks/Task1.java
+++ javaPart2Practice/src/main/java/com/edu/threadpool/tasks/Task1.java
@@ -25,17 +25,19 @@ public class Task1 {
         ExecutorService threadExecutor = Executors.newFixedThreadPool(2);
         ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNextInt()){
+        while (scanner.hasNextInt()) {
             int seconds = scanner.nextInt();
-            if(seconds < 0) {
+            if (seconds < 0) {
                 break;
             }
             threadExecutor.submit(() -> {
                 Integer counter = threadLocal.get();
                 threadLocal.set(counter == null ? 1 : ++counter);
-                System.out.println(String.format("Поток '%s', задач: '%d'", Thread.currentThread().getName(), threadLocal.get()));
+                System.out.println(String.format("Поток '%s', задач: '%d'",
+                        Thread.currentThread().getName(), threadLocal.get()));
                 Thread.sleep(seconds * 1000);
-                System.out.println(String.format("Поток '%s' спал '%d' секунд", Thread.currentThread().getName(), seconds));
+                System.out.println(String.format("Поток '%s' спал '%d' секунд",
+                        Thread.currentThread().getName(), seconds));
                 return seconds;
             });
         }
